779812b995eaa2da6ea1f1ae3aedc24ef95bb151
Anuj Deshmukh <84030389+iAnujDeshmukh@users.noreply.github.com>
2025-05-19 15:27:55 +0000

    datatypes added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS as newer version
+
+let userName = "abc" //string
+let userId   =  123  //Number
+let isHappt  =  true //Boolean
+
+//number => 2^53
+// string => use double quotes ""
+// boolean => true/false
+// null => is a standalone value and a empty value
+// undefined => nothing assigned and is a datatype initself
+// Object
+
+console.log(typeof(userName)) //string
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
